0d5d0a8dbb2264a9f053fd254fe1ee25009cfb81
Anatoly Parfenov <anatoly.parfenov@mail.ru>
2023-02-17 00:36:07 +0500

    Added choise of games. It's 5 step in our project.

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,18 @@
 package hexlet.code;
+import java.util.Scanner;
+
 
 public class App {
     public static void main(String[] args) {
-
-        System.out.println("Welcome to the Brain Games!");
+        System.out.println("Please enter the game number and press Enter." + "\n" + "1 - Greet" + "\n" + "0 - Exit");
+        Scanner app = new Scanner(System.in);
+        var option = app.nextInt();
+        if(option == 0) {
+            System.exit(0);
+        } else {
+            System.out.println("Your choice:" + option);
+        }
         Cli.run();
+        app.close();
     }
 }
--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,8 +3,11 @@ import java.util.Scanner;
 public class Cli {
     public static void run() {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to the Brain Games!");
         System.out.println("May I have your name?");
         String name = sc.nextLine();
         System.out.println("Hello, " + name + "!");
+        sc.close();
     }
 }
+
