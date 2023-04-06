2eff9d788db80f7dd0b22305c6dc59923b18a57c
Anatoly Parfenov <anatoly.parfenov@mail.ru>
2023-04-06 21:33:01 +0500

    Fixed remarks. All dome.

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -3,6 +3,8 @@ package hexlet.code.games;
 import hexlet.code.Engine;
 import hexlet.code.RandomUtils;
 
+import java.math.BigInteger;
+
 public class Prime {
 
     private static final String QUESTION_PRIME = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
@@ -13,29 +15,17 @@ public class Prime {
     private static String[][] gameLogic() {
         String[][] data = new String[Engine.ROUND_COUNT][Engine.ANSWER_VARIANTS];
         for(int i = 0; i < Engine.ROUND_COUNT; i++) {
-            int randomNegativeOrPositiveNumber = RandomUtils.generateNumber(0, 2);
             int randomNumber = RandomUtils.generateNumber();
-            int correctAnswer = randomNegativeOrPositiveNumber == 1 ? randomNumber : randomNumber * (-1);
-            data[i][0] = String.valueOf(correctAnswer);
-            data[i][1] = isPrime(correctAnswer) ? "yes" : "no";
+            data[i][0] = String.valueOf(randomNumber);
+            data[i][1] = isPrime(randomNumber) ? "yes" : "no";
         }
         return data;
 
     }
 
     private static boolean isPrime(int correctAnswer) {
-        if (correctAnswer == 2) {
-            return true;
-        }
-        if (correctAnswer < 0 || correctAnswer == 1 || correctAnswer % 2 == 0) {
-            return false;
-        }
-        for (int i = 2; i <= Math.sqrt(correctAnswer); i++) {
-            if(correctAnswer % i == 0) {
-                return false;
-            }
-        }
-        return true;
+        BigInteger bigInteger = BigInteger.valueOf(correctAnswer);
+        return bigInteger.isProbablePrime((int) Math.log(correctAnswer));
     }
 }
 
