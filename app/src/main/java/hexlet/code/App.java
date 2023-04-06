723dfcff35adfc57cce628ddd1758455b9118a3a
Anatoly Parfenov <anatoly.parfenov@mail.ru>
2023-04-06 22:21:08 +0500

    Fixed remarks. All dome.

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,20 +1,24 @@
 package hexlet.code;
 
-import hexlet.code.games.*;
+import hexlet.code.games.Calculator;
+import hexlet.code.games.Even;
+import hexlet.code.games.GCD;
+import hexlet.code.games.Prime;
+import hexlet.code.games.Progression;
 
 import java.util.Scanner;
 
 
 public class App {
-    public static int GAME;
-    public static final int EXIT = 0;
+    private static int game;
+    private static final int EXIT = 0;
 
-    public static final int GREET = 1;
-    public static final int EVEN = 2;
-    public static final int CALCULATOR = 3;
-    public static final int GREATEST_COMMON_DIVISOR = 4;
-    public static final int PROGRESSION = 5;
-    public static final int PRIME = 6;
+    private static final int GREET = 1;
+    private static final int EVEN = 2;
+    private static final int CALCULATOR = 3;
+    private static final int GREATEST_COMMON_DIVISOR = 4;
+    private static final int PROGRESSION = 5;
+    private static final int PRIME = 6;
 
 
 
@@ -30,34 +34,34 @@ public class App {
                 6 - Prime
                 0 - Exit""");
         Scanner app = new Scanner(System.in);
-        GAME = app.nextInt();
-        switch (GAME) {
+        game = app.nextInt();
+        switch (game) {
             case GREET -> {
-                System.out.println("Your choice:" + GAME + "\n");
+                System.out.println("Your choice:" + game + "\n");
                 Cli.run();
             }
             case EVEN -> {
-                System.out.println("Your choice:" + GAME + "\n");
+                System.out.println("Your choice:" + game + "\n");
                 Even.parity();
             }
             case CALCULATOR -> {
-                System.out.println("Your choice:" + GAME + "\n");
+                System.out.println("Your choice:" + game + "\n");
                 Calculator.calc();
             }
             case GREATEST_COMMON_DIVISOR -> {
-                System.out.println("Your choice:" + GAME + "\n");
+                System.out.println("Your choice:" + game + "\n");
                 GCD.gcd();
             }
             case PROGRESSION -> {
-                System.out.println("Your choice:" + GAME + "\n");
+                System.out.println("Your choice:" + game + "\n");
                 Progression.progression();
             }
             case PRIME -> {
-                System.out.println("Your choice:" + GAME + "\n");
+                System.out.println("Your choice:" + game + "\n");
                 Prime.prime();
             }
             case EXIT -> {
-                System.out.println("Your choice:" + GAME + "\n");
+                System.out.println("Your choice:" + game + "\n");
                 System.exit(0);
             }
             default -> {
